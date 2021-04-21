bc053f82c3fc5066e93c8d6e0d1af5ff4469b3fc
Gal Mizrahi <galssd@Gals-iMac.lan>
2021-04-21 15:37:05 +0300

    production initial

--- config/db.js
+++ config/db.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
+    // host:"localhost",
+    // user:"root",
+    // password:"",
+    // database:"movies"
+    host:procces.env.HOST,
+    user:procces.env.USER,
     password:"",
-    database:"movies"
+    database:procces.env.DATABASE
 
 })
 
